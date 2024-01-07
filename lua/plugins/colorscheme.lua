2af1f14d29701ca34ee6f1d080b4311685df22dc
ray <something.ray97@gmail.com>
2024-01-07 19:44:29 +0800

    initial conf

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  -- { "ellisonleao/gruvbox.nvim" },
+  -- { "sainnhe/gruvbox-material" },
+  { "sainnhe/everforest" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "gruvbox",
+      -- colorscheme = "gruvbox-material",
+      colorscheme = "everforest",
+    },
+  },
+}
---
+++ lua/plugins/pbundle.lua
@@ -0,0 +1,12 @@
+return {
+  "szw/vim-maximizer",
+  "tpope/vim-surround", -- add, delete, change surroundings (it's awesome)
+  "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+  "numToStr/Comment.nvim",
+  "windwp/nvim-autopairs", -- autoclose parens, brackets, quotes, etc...
+  "windwp/nvim-ts-autotag", -- autoclose tags
+
+  -- Suggested by Jon Gjengset
+  "dense-analysis/ale",
+  "machakann/vim-highlightedyank",
+}
